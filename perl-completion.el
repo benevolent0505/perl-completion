798aa620eb8dc2f3704d4a80c3dd4b4cfea4ae13
benevolent0505 <mikio.fjt@gmail.com>
2018-04-15 20:24:19 +0900

    fix

--- perl-completion.el
+++ perl-completion.el
@@ -1309,8 +1309,8 @@ point, after `plcmp-re-search-forward-fontify'")
                       (and (stringp name)
                            (insert name)))))
                  ))))
-(setq helm-type-command
-			(append helm-type-command '(("Completion" . plcmp-type-completion))))
+(setq helm-type-command-actions
+			(append helm-type-command-actions '(("Completion" . plcmp-type-completion))))
 
 (defvar plcmp-type-completion-method
     '(plcmp-completion-method
@@ -1335,8 +1335,8 @@ point, after `plcmp-re-search-forward-fontify'")
                       (and (stringp name)
                            (insert name)))))
                  ))))
-(setq helm-type-command
-			(append helm-type-command '(("Completion-method" . plcmp-type-completion-method))))
+(setq helm-type-command-actions
+			(append helm-type-command-actions '(("Completion-method" . plcmp-type-completion-method))))
 
 (defvar plcmp-type-man
   '(plcmp-doc
@@ -1359,8 +1359,8 @@ point, after `plcmp-re-search-forward-fontify'")
                                         (regexp-quote candidate)))))
                ("Insert man name" . insert)
                ("Add man name to kill-ring" . kill-new)))))
-(setq helm-type-command
-			(append helm-type-command '(("Man" . plcmp-type-man))))
+(setq helm-type-command-actions
+			(append helm-type-command-actions '(("Man" . plcmp-type-man))))
 
 (defvar plcmp-type-perldoc
   '(plcmp-perldoc
